198c363cab1aabd3e8c6e171ea8f603a893a234b
DeviJv <septian.ardi.wijaya@gmail.com>
2026-03-26 17:10:56 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,37 +1,37 @@
-import { MetadataRoute } from 'next';
+// import { MetadataRoute } from 'next';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://e-commerce-plant.vercel.app'; // Ganti jika sudah ada domain tetap
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://e-commerce-plant.vercel.app'; // Ganti jika sudah ada domain tetap
+//   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-  try {
-    // 1. Ambil data slug dari Laravel API
-    const response = await fetch(`${apiUrl}/api/sitemap-data`, { cache: 'no-store' });
-    if (!response.ok) {
-        return [{ url: baseUrl, lastModified: new Date() }];
-    }
-    const { data } = await response.json();
+//   try {
+//     // 1. Ambil data slug dari Laravel API
+//     const response = await fetch(`${apiUrl}/api/sitemap-data`, { cache: 'no-store' });
+//     if (!response.ok) {
+//         return [{ url: baseUrl, lastModified: new Date() }];
+//     }
+//     const { data } = await response.json();
 
-    // 2. Format data untuk sitemap Next.js
-    const dynamicRoutes = data.map((item: any) => ({
-      url: `${baseUrl}${item.url}`,
-      lastModified: new Date(item.last_modified),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    }));
+//     // 2. Format data untuk sitemap Next.js
+//     const dynamicRoutes = data.map((item: any) => ({
+//       url: `${baseUrl}${item.url}`,
+//       lastModified: new Date(item.last_modified),
+//       changeFrequency: 'weekly',
+//       priority: 0.8,
+//     }));
 
-    // 3. Gabungkan dengan static routes
-    return [
-      {
-        url: baseUrl,
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-        priority: 1,
-      },
-      ...dynamicRoutes,
-    ];
-  } catch (error) {
-    console.error("Sitemap generation error:", error);
-    return [{ url: baseUrl, lastModified: new Date() }];
-  }
-}
+//     // 3. Gabungkan dengan static routes
+//     return [
+//       {
+//         url: baseUrl,
+//         lastModified: new Date(),
+//         changeFrequency: 'daily',
+//         priority: 1,
+//       },
+//       ...dynamicRoutes,
+//     ];
+//   } catch (error) {
+//     console.error("Sitemap generation error:", error);
+//     return [{ url: baseUrl, lastModified: new Date() }];
+//   }
+// }
